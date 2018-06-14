8d20a65869142866331fc0a2bd1992ded616bce4
Guo <albert.guo@sap.com>
2018-06-13 19:15:56 -0700

    adding files

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FoodItem }  from './app.foodItem';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'app';
+  items;
+
+  constructor() {
+    if (sessionStorage.getItem("autosave")) {
+      this.items = sessionStorage.getItem("autosave");
+    } else {
+      this.items = FOODITEMS;
+    }
+  }
 }
+var FOODITEMS: FoodItem[] = [
+  { id: 1, name: "Cucumbers", mfg: "Hot House", pkg: "10 KG", qty: 6 },
+  { id: 2, name: "Carrots", mfg: "Hot House", pkg: "8 KG", qty: 14 },
+  { id: 3, name: "Cheese", mfg: "Hot House", pkg: "5 KG", qty: 20 },
+  { id: 4, name: "French Fries", mfg: "McCain", pkg: "15 KG", qty: 8 }  
+];
---
+++ myapp/src/app/app.foodItem.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+// This is my FoodItem class.
+export class FoodItem {
+    id:   number;
+    name: string;
+    mfg:  string;
+    pkg:  string;
+    qty:  number;
+}
